800ceaa28beeb443d9d9ccfbb296ecc2e2727b90
bderrickmatthew <bderrickmatthew@gmail.com>
2024-02-05 20:13:10 +0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+
+$title = "Ecclessia Church Management System";
+
+ob_start();
+
+include __DIR__ . '/templates/home.html.php';
+
+$output = ob_get_clean();
+
+include __DIR__ . '/templates/layout.html.php';
