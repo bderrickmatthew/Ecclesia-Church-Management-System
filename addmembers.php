1666cf16ca8d21f8f584bcdb9353988904388e20
bderrickmatthew <bderrickmatthew@gmail.com>
2024-03-04 20:33:12 +0300

    Update addmembers.php
    added the include files that contains the functions needed.
    use of the function insertMember

--- addmembers.php
+++ addmembers.php
@@ -1,34 +1,34 @@
 <?php
 
-if ($_SERVER['REQUEST_METHOD'] == "POST" && isset($_POST['submit'])) {
-	
-	try {
-		
-		$pdo = new PDO('mysql:host=localhost;dbname=ecms;charset=utf8', 'root', '');
+if ($_SERVER['REQUEST_METHOD'] == "POST" && isset($_POST['submit']))
+{
 
-		$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+	try
+	{
 
-		// insert data into sql
-		$sql = 'INSERT INTO `members` SET `first_name` = :first_name, `middle_name` = :middle_name, `last_name` = :last_name, `email` = :email, `gender` = :gender, `occupation` = :occupation, `phone_num` = :phone_num, `date_of_birth` = :date_of_birth, `address` = :address, `membership_status` = :membership_status';
+		include __DIR__ . '/includes/DatabaseConnection.php';
+		include __DIR__ . '/includes/DatabaseFunctions.php';
 
-		$stmt = $pdo->prepare($sql);
-		$stmt->bindValue(':first_name', $_POST['first_name']); 
-		$stmt->bindValue(':middle_name', $_POST['middle_name']); 
-		$stmt->bindValue(':last_name', $_POST['last_name']);
-		$stmt->bindValue(':email', $_POST['email']);
-		$stmt->bindValue(':gender', $_POST['gender']); 
-		$stmt->bindValue(':occupation', $_POST['occupation']); 
-		$stmt->bindValue(':phone_num', $_POST['phone_num']);
-		$stmt->bindValue(':date_of_birth', $_POST['date_of_birth']); 
-		$stmt->bindValue(':address', $_POST['address']);
-		$stmt->bindValue(':membership_status', $_POST['membership_status']);
-		$stmt->execute();
+		insertMember($pdo,
+		 $_POST['first_name'],
+		 $_POST['middle_name'],
+		 $_POST['last_name'],
+		 $_POST['email'],
+		 $_POST['gender'],
+		 $_POST['occupation'],
+		 $_POST['phone_num'],
+		 $_POST['date_of_birth'],
+		 $_POST['address'],
+		 $_POST['membership_status']
+		);
 
 		header('location: members.php');
 
 
-	} catch (Exception $e) {
-		$title = 'An error has occurred';
+	}
+	catch (Exception $e)
+	{
+		$title  = 'An error has occurred';
 		$output = 'Database error: ' . $e->getMessage() . ' ' . $e->getFile() . ':' . $e->getLine();
 	}
 }
