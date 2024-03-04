e5b10db2cccaa7822db04080bfc821129e9302bb
bderrickmatthew <bderrickmatthew@gmail.com>
2024-03-04 20:20:40 +0300

    Update DatabaseFunctions.php
    added function query

--- includes/DatabaseFunctions.php
+++ includes/DatabaseFunctions.php
@@ -1,17 +1,32 @@
 <?php
-	
-	/**
-	 * totalMembers
-	 *
-	 * @param  mixed $database
-	 * @return void
-	 */
-	function totalMembers($database)
-	{
-		$query = $database->prepare('SELECT COUNT(*) FROM `members`');
-		$query->execute();
 
-		$row = $query->fetch();
+/**
+ * totalMembers
+ *
+ * @param  mixed $database
+ * @return void
+ */
+function totalMembers($database)
+{
+    $query = $database->prepare('SELECT COUNT(*) FROM `members`');
+    $query->execute();
 
-		return $row[0];
-	}
+    $row = $query->fetch();
+
+    return $row[0];
+}
+
+/**
+ * query
+ *
+ * @param  mixed $pdo
+ * @param  mixed $sql
+ * @param  mixed $parameters
+ * @return void
+ */
+function query($pdo, $sql, $parameters = [])
+{
+    $query = $pdo->prepare($sql);
+    $query->execute($parameters);
+    return $query;
+}
